b3a77e83bd219c630b45737f412c50b9242d193b
NataliaNavasC <natalia.navas.ca@gmail.com>
2024-04-05 07:37:34 -0500

    Documentation v3

--- calculator/src/main/java/com/crpypst/Calculator.java
+++ calculator/src/main/java/com/crpypst/Calculator.java
@@ -20,8 +20,7 @@ public class Calculator {
         }
         return -1; // Error flag
     }
-
     public double plus2( double a ){
-        return a + 2;
+        return a + 2; // test
     }
 }
